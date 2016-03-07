9446b52bea454c04fff0cba2d69ea90fe5005f85
SiaraQ <siaraq@gmail.com>
2016-03-07 18:05:35 +0100

    Swimming widget
    add

--- js/gridster.js
+++ js/gridster.js
@@ -5,6 +5,7 @@ var gridster;
 					widget_margins: [10, 10], 
 					widget_base_dimensions: [190, 210],
 					autogrow_cols: true,
+                    shift_widgets_up: false,
 					resize: {
 		              enabled: true,
                 }
